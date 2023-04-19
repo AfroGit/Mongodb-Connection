b4a37f7b40bb41fa8153939025f81777c02295ae
AfroGit <17697323-AfroGit@users.noreply.replit.com>
2023-04-19 21:49:55 +0000

    Used mongoose for the first time

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const dbConnect = 'mongodb+srv://KujengA4222:rxcx6NpjCK05sRWk@cluster0.lhyk1vq.mongodb.net/?retryWrites=true&w=majority'
+
+async function connect(){
+  try{
+    await mongoose.connect(dbConnect);
+    console.log('Connected To MongoDB');
+  } catch(error) {
+    console.log('Error')
+  }
+}
 
 
-
+connect();
 
 app.listen(2023, () => {
-  console.log('Connected To MongoDB')
+  console.log('Server is Running On Port 2023')
 })
